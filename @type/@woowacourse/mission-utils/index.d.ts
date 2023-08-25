ac7d406d10c0ca5d5f58be30afba487abc88a1f8
cochae <huisu121@gmail.com>
2023-08-26 01:11:00 +0900

    docs: 함수의 매개변수명 수정

--- @type/@woowacourse/mission-utils/index.d.ts
+++ @type/@woowacourse/mission-utils/index.d.ts
@@ -1,6 +1,6 @@
 declare module '@woowacourse/mission-utils' {
   class Console {
-    static readLine(query: string, callback: Function): void;
+    static readLine(message: string, callback: Function): void;
     static close(): void;
     static print(message: string): void;
   }
