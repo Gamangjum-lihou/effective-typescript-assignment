bafdf92406803299231bb91b5fb1880c255e7c38
cochae <huisu121@gmail.com>
2023-08-25 16:02:49 +0900

    feat: 출력 메세지 상수로 정의

---
+++ src/constants/Message.ts
@@ -0,0 +1,40 @@
+const ERROR = '[ERROR]';
+
+const JUDGE = Object.freeze({
+  ball: '볼',
+  strike: '스트라이크',
+  nothing: '낫싱'
+});
+
+const COMMAND = Object.freeze({
+  retry: '1',
+  quit: '2'
+});
+
+const INPUT_MESSAGE = Object.freeze({
+  answer: '숫자를 입력해주세요 : ',
+  ask_retry_or_quit: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
+});
+
+const OUTPUT_MESSAGE = Object.freeze({
+  start: '숫자 야구 게임을 시작합니다.',
+  end: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  ball: (count: number) => `${count}${JUDGE.ball}`,
+  strike: (count: number) => `${count}${JUDGE.strike}`,
+  nothing: `${JUDGE.nothing}`,
+  gap: ' '
+});
+
+const ERROR_MESSAGE = Object.freeze({
+  number_digit: `${ERROR} 서로 다른 임의의 수 3개를 입력해주세요.`,
+  retry_or_quit: `${ERROR} '${COMMAND.retry}'(새로 시작) 또는 '${COMMAND.quit}'(게임 종료)를 입력해주세요.`
+});
+
+module.exports = {
+  ERROR,
+  JUDGE,
+  COMMAND,
+  INPUT_MESSAGE,
+  OUTPUT_MESSAGE,
+  ERROR_MESSAGE
+};
