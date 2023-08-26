445ef5866d1b2f127485b419b368702c3d79dd46
ryan-dia <cjfdnjs158@naver.com>
2023-08-26 17:00:25 +0900

    feat: add Controller

---
+++ src/Controller/index.ts
@@ -0,0 +1,43 @@
+//import Validator from '../Validator';
+import View from '../View';
+import Model from '../Model/index';
+import runGenerator from '../utils/runGenerator';
+import { SELECT_FINISH } from '../constants/System';
+
+class BaseballController {
+  #view: typeof View;
+  #model: Model;
+
+  constructor() {
+    this.#view = View;
+    this.#model = new Model();
+  }
+
+  play() {
+    this.#view.printStart();
+    runGenerator(this.#run.bind(this));
+  }
+
+  *#run(): Generator<any> {
+    this.#model.setGame();
+    while (true) {
+      const userNumbers = yield this.#view.readGameNumbers;
+      const score = this.#model.compareUserWithComputerNumbers(userNumbers as string);
+      this.#view.printHint(score);
+      if (this.#model.isThreeStrikes()) {
+        this.#view.printSuccess();
+        break;
+      }
+    }
+
+    const isFinish = yield this.#view.readGameCommand;
+    if (isFinish === SELECT_FINISH) {
+      this.#view.finishGame();
+      return;
+    }
+
+    yield* this.#run();
+  }
+}
+
+export default BaseballController;
