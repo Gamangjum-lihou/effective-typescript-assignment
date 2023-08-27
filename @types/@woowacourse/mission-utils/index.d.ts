46ea3627daaef1d78689447ea68d4aa991dd6488
cochae <huisu121@gmail.com>
2023-08-27 19:59:40 +0900

    chore: d.ts 에 정의한 매개변수 중 Function 타입 구체화

--- @types/@woowacourse/mission-utils/index.d.ts
+++ @types/@woowacourse/mission-utils/index.d.ts
@@ -2,7 +2,7 @@ declare module '@woowacourse/mission-utils' {
   class Console {
     static close(): void;
     static print(message: string): void;
-    static readLine(input: string, callback: Function): void;
+    static readLine(input: string, callback: (input: string) => void): void;
   }
 
   class Random {
