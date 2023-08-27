af4f7807597233ddc236278d3026d1b1cb7baea7
cochae <huisu121@gmail.com>
2023-08-27 10:12:19 +0900

    chore: d.ts 에 정의된 메세드의 매개변수명 수정

--- @type/@woowacourse/mission-utils/index.d.ts
+++ @type/@woowacourse/mission-utils/index.d.ts
@@ -2,7 +2,7 @@ declare module '@woowacourse/mission-utils' {
   class Console {
     static close(): void;
     static print(message: string): void;
-    static readLine(message: string, callback: Function): void;
+    static readLine(input: string, callback: Function): void;
   }
 
   class Random {
