62342fcdee85fe1528fd43705a4bb62324241198
ryan-dia <cjfdnjs158@naver.com>
2023-09-03 04:34:34 +0900

    chore: set linter & formatter

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['eslint:recommended', 'prettier', 'hijacking-ts'],
+  rules: {
+    semi: 'off',
+    '@typescript-eslint/semi': 'error',
+    // 들여쓰기 깊이 제한
+    'max-depth': ['error', 2],
+    // 함수의 매개변수 개수 제한
+    'max-params': ['error', 4],
+  },
+  ignorePatterns: ['.eslintrc.js'],
+};
